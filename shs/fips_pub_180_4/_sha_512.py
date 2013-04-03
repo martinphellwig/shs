85bf7da92d68bb2de76a3369e468939958b48b0f
hellwig <hellwig@localhost>
2013-04-03 14:27:10 +0100

    Update to FIPS 180-4; Initial Framework, SHA384 & SHA512 revisit.

--- shs/fips_pub_180_4/_sha_512.py
+++ shs/fips_pub_180_4/_sha_512.py
@@ -17,44 +17,46 @@ SIZE_WORD = 64
 SIZE_BLOCK = 1024
 
 def rotate_right(word, amount):
+    # 2.2.2 Symbols and Operations # ROTRn(x)
     binary = word._binary
     rotate = binary[-amount:] + binary[:-amount] 
     return_value = Bits(rotate, SIZE_WORD)
     return(return_value)
 
 def shift_right(word, amount):
+    # 2.2.2 Symbols and Operations # SHRn(x)
     binary = word._binary
     shift = binary[:-amount]
     return_value = Bits(shift, SIZE_WORD)
     return(return_value)
 
 def function_ch(b, c, d):
-    # (4.1.3)
+    # 4.1.3 - (4.8)
     value = (b & c) | ((~ b) & d)
     return(value)
 
 def function_maj(b, c, d):
-    # (4.1.3)
+    # 4.1.3 - (4.9)
     value = (b & c) | (b &d ) | (c & d)
     return(value)
 
 def sigma_upper_512_0(x):
-    # (4.1.3)
+    # 4.1.3 - (4.10)
     value = rotate_right(x, 28) ^ rotate_right(x, 34) ^ rotate_right(x, 39)
     return(value)
 
 def sigma_upper_512_1(x):
-    # (4.1.3)
+    # 4.1.3 - (4.11)
     value = rotate_right(x, 14) ^ rotate_right(x, 18) ^ rotate_right(x, 41)
     return(value)
 
 def sigma_lower_512_0(x):
-    # (4.1.3)
+    # 4.1.3 - (4.12)
     value = rotate_right(x, 1) ^ rotate_right(x, 8) ^ shift_right(x, 7)
     return(value)
 
 def sigma_lower_512_1(x):
-    # (4.1.3)
+    # 4.1.3 - (4.13)
     value = rotate_right(x, 19) ^ rotate_right(x, 61) ^ shift_right(x, 6)
     return(value)
 
@@ -83,6 +85,7 @@ def computation(message):
         # - Fill with 80 64 bits word values
         message_schedule = dict()
         # This set's up the first 16 words, using the words in the block
+        # see the 0 <=t<=15 part of the specification
         for count, word in enumerate(block.split(SIZE_WORD)):
             word = Bits(word, SIZE_WORD)
             message_schedule[count] = word
@@ -91,7 +94,7 @@ def computation(message):
         for index in range(16, 80):
             # Implementation Note: 
             # The following code-block is an on-liner in the documentation,
-            # see the 16<=t<=79 part of the equation
+            # see the 16 <=t<=79 part of the specification
             w02 = message_schedule[index-2]
             w07 = message_schedule[index-7]
             w15 = message_schedule[index-15]
@@ -114,6 +117,7 @@ def computation(message):
         h = h7
             
         # Step 3. 80 rounds of calculation
+        # For t=0 to 79:
         for index in range(80):
             
             kt = Bits(K_SHA_512[index], SIZE_WORD)
@@ -136,13 +140,13 @@ def computation(message):
             a = t1 + t2
         
         # Step 4. compute the intermediate hash value    
-        h0 = (h0 + a)
-        h1 = (h1 + b)
-        h2 = (h2 + c)
-        h3 = (h3 + d)
-        h4 = (h4 + e)
-        h5 = (h5 + f)
-        h6 = (h6 + g)
-        h7 = (h7 + h)
+        h0 = a + h0
+        h1 = b + h1
+        h2 = c + h2
+        h3 = d + h3
+        h4 = e + h4
+        h5 = f + h5
+        h6 = g + h6
+        h7 = h + h7
     
     return(h0, h1, h2, h3, h4, h5, h6, h7)
